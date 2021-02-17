f55a2d14ef9dcd9ef8be7e694a96fd3a134c8680
roy yoo <royyoo123@gmail.com>
2021-02-17 13:48:14 -0800

    done with airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Small cozy place',
+  address: '102 Harmony Gardens London h3 12h',
+  description: 'A homely place for homely people',
+  price_per_night: 120,
+  number_of_guests: 5
+)
+Flat.create!(
+  name: 'Haunted house',
+  address: '1250 Godbey Gardens London W9 1DT',
+  description: 'A haunted house. Many people were murdered here with a dark history',
+  price_per_night: 10000,
+  number_of_guests: 100
+)
+Flat.create!(
+  name: 'Castle',
+  address: '10555 Castle Gardens London W9 1DT',
+  description: 'A refurbished castle',
+  price_per_night: 755,
+  number_of_guests: 35
+)
